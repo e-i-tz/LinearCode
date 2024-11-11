47cbd064872f7e1e2e39c24037fa6e899b4393bb
Mariendorf Jaclynne <Procrastinatqr@yandex.com>
2024-11-11 21:04:39 +0300

    Написал функцию, получающую проверочную матрицу H

--- GeneratorAndCheckMatrix.py
+++ GeneratorAndCheckMatrix.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 # Создает генераторную матрицу G размером k x n в систематическом виде [I | A].
 def create_generator_matrix(k, n, d):
     # Строит I, генерирует A и проверяет линейную независимость строк в получившейся G, повторяя процесс при необходимости.
@@ -16,12 +15,18 @@ def create_generator_matrix(k, n, d):
             min_weight = np.min(np.sum(G, axis=1))
             if min_weight == d:
                 break
-    return G
+    return G, A
 
-# Пример использования:
-k = 4
-n = 7
-d = 4
-G = create_generator_matrix(k, n, d)
-print("Генераторная матрица G в систематическом виде:")
-print(G)
+
+def create_parity_check_matrix(A):
+    I = np.eye(A.shape[1], dtype=int)
+    A = A.T
+    H = np.hstack((A, I))
+    return H
+
+
+
+
+
+
+
--- LinearCode.py
+++ LinearCode.py
@@ -1,4 +1,5 @@
 from InputParametrs import get_code_parameters, validate_code_parameters
+from GeneratorAndCheckMatrix import create_generator_matrix, create_parity_check_matrix
 
 n, k, d = get_code_parameters()
 
@@ -8,3 +9,10 @@ try:
 except ValueError as e:
     print(e)
 
+G, A = create_generator_matrix(k, n, d)
+H = create_parity_check_matrix(A)
+
+print("Генераторная матрица G в систематическом виде:")
+print(G)
+print("Проверочная матрица H:")
+print(H)
